6bef8561086f7e81dd7c4374cc214893cdbc06aa
seyoi <seyoit@gmail.com>
2025-01-08 18:55:47 +0900

    aslkdjf

--- front/next.config.ts
+++ front/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // 빌드 중 ESLint 오류 무시
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
