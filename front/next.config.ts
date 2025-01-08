75e908ade74e7e1938446df96ede5bd4c8a90221
seyoi <seyoit@gmail.com>
2025-01-08 21:33:35 +0900

    dkfjs

--- front/next.config.ts
+++ front/next.config.ts
@@ -2,7 +2,6 @@ import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   eslint: {
-    // 빌드 중 ESLint 오류 무시
     ignoreDuringBuilds: true,
   },
 
@@ -11,7 +10,11 @@ const nextConfig: NextConfig = {
       {
         source: "/(.*)", // 모든 경로에 대해 설정
         headers: [
-          
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; object-src 'none'; base-uri 'self'; form-action 'self';", // unsafe-eval 허용
+          },
           {
             key: "Access-Control-Allow-Origin",
             value: "*", // 모든 출처에 대해 CORS 허용
