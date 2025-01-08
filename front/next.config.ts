ad38aa374eb746d7050d1f209b1d9e4d607f1e6c
seyoi <seyoit@gmail.com>
2025-01-08 21:05:55 +0900

    asdfk

--- front/next.config.ts
+++ front/next.config.ts
@@ -11,11 +11,7 @@ const nextConfig: NextConfig = {
       {
         source: "/(.*)", // 모든 경로에 대해 설정
         headers: [
-          {
-            key: "Content-Security-Policy",
-            value:
-              "default-src 'self'; script-src 'self' 'unsafe-inline'; object-src 'none'; base-uri 'self'; form-action 'self';", // unsafe-eval 제거
-          },
+          
           {
             key: "Access-Control-Allow-Origin",
             value: "*", // 모든 출처에 대해 CORS 허용
